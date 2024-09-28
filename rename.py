bd797e94caa7d7b32e9aada2c31b23a7445b5b16
hansmeuller <118677299+hansmeuller@users.noreply.github.com>
2024-09-28 08:25:01 +0200

    path + extract

--- rename.py
+++ rename.py
@@ -1,9 +1,25 @@
 from PIL import Image
 import pytesseract
-
 import os
-
 import pdf2image
-
 import re
 
+# scant im verzeichnis in dem python file liegt
+scans_folder = os.path.dirname(os.path.realpath(__file__))
+
+def process_scan_files(scans_folder):
+    """scan verzeichnis und bearbeite """
+    for file_name in os.listdir(scans_folder):
+        if file_name.endswith((".png", ".jpg", ".pdf")):
+            file_path = os.path.join(scans_folder, file_name)
+            print(f"verarbeite {file_name}...")
+
+
+def extract_text_from_image(image_path):
+    """extrahiere text"""
+    try:
+        with Image.open(image_path) as img:
+            return pytesseract.image_to_string(img)
+    except Exception as e:
+        print(f"Erreur de traitement de l'image {image_path}: {e}")
+        return ""
